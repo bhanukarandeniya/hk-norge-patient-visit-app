fc1a4bab107a36487080c8644abde5b1d9eec6b8
bhanukarandeniya <bhanukarandeniya@gmail.com>
2021-05-29 19:22:37 +0530

    Custom exception handler implementation

--- src/main/java/com/norge/patientvisit/controller/errors/ErrorConstants.java
+++ src/main/java/com/norge/patientvisit/controller/errors/ErrorConstants.java
@@ -4,10 +4,14 @@ import java.net.URI;
 
 public class ErrorConstants {
 
+    private ErrorConstants() {
+    }
+
     public static final URI DEFAULT_TYPE = URI.create("/problem-with-message");
     public static final URI ERR_HOLIDAY = URI.create("");
     public static final URI ERR_DUPLICATION = URI.create("");
+    public static final String HOLIDAY_ENTITY_CREATE_ERROR = "Entity cannot be created on a holiday";
+    public static final String HOLIDAY_ENTITY_MODIFY_ERROR = "Entity cannot be modified on a holiday";
+    public static final String ENTITY_DUPLICATION_ERROR = "Entity with the given id already exist";
 
-    private ErrorConstants() {
-    }
 }
